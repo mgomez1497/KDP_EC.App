01f358691f2251809b91d93473918ce694cc5d2d
mgomez1497 <150944556+mdatafarm@users.noreply.github.com>
2025-07-21 11:43:28 -0500

    AddBalanceCostCentersRep

---
+++ Infraestructure/Repository/SQLite/BalanceCostCentersRepository.cs
@@ -0,0 +1,51 @@
+﻿using KDP_EC.App.Infraestructure.Database;
+using KDP_EC.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KDP_EC.App.Infraestructure.Repository.SQLite
+{
+    public class BalanceCostCentersRepository:SqLiteConnection
+    {
+        private readonly HttpClient _httpClient;
+
+        public BalanceCostCentersRepository()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task InitializeAsync()
+        {
+            await base.InitAsync();
+            await _db.CreateTableAsync<BalanceCostCenters>();
+        }
+
+        public async Task SaveBalanceCostCentersLocally(List<BalanceCostCenters> balanceCostCenters)
+        {
+            await InitializeAsync();
+            await _db.DeleteAllAsync<BalanceCostCenters>();
+            await _db.InsertAllAsync(balanceCostCenters);
+        }
+
+
+        public async Task<List<BalanceCostCenters>> GetLocalBalanceCostCentersById()
+        {
+            await InitializeAsync();
+            return await _db.Table<BalanceCostCenters>().ToListAsync();
+        }
+
+
+        public async Task<List<BalanceCostCenters>> GetLocalBalanceCostCentersByFarmIdAndYear(Guid FarmId, int year)
+        {
+            await InitializeAsync();
+            var localBalanceCostCenters = await _db.Table<BalanceCostCenters>()
+                                                   .Where(s => s.FarmId == FarmId && s.Year == year)
+                                                   .ToListAsync();
+            return localBalanceCostCenters;
+        }
+
+    }
+}
---
+++ Infraestructure/Repository/Sincronizar/BalanceCostCentersRepositoryAPI.cs
@@ -0,0 +1,35 @@
+﻿using KDP_EC.App.Infraestructure.API;
+using KDP_EC.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KDP_EC.App.Infraestructure.Repository.Sincronizar
+{
+    public class BalanceCostCentersRepositoryAPI
+    {
+        private readonly ConsumoApi _api;
+
+        public BalanceCostCentersRepositoryAPI()
+        {
+            _api = new ConsumoApi("https://localhost:7149/api/BalanceCostCenters/");
+
+        }
+
+        public async Task<List<BalanceCostCenters>> ObtenerBalanceCC(Guid FarmId)
+        {
+            try
+            {
+                var balanceCostCentersResult = await _api.GetAsync<List<BalanceCostCenters>>($"getBalanceCostCentersByFarm?farmId={FarmId}");
+                return balanceCostCentersResult ?? new List<BalanceCostCenters>();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"❌ Error al obtener el balance de centros de costo: {ex.Message}");
+                return new List<BalanceCostCenters>();
+            }
+        }
+    }
+}
